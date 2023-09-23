15248a8e288b0d48c88848b8722628015d554c91
LinkCs5 <90697790+LinkCs5@users.noreply.github.com>
2023-09-23 06:58:50 -0600

    Add files via upload

---
+++ Practica_7/Program.cs
@@ -0,0 +1,75 @@
+﻿Console.Write("Ingresa el número de filas: ");
+    if (int.TryParse(Console.ReadLine(), out int filas) && filas > 0)
+    {
+        Console.Write("Ingresa el número de columnas: ");
+        if (int.TryParse(Console.ReadLine(), out int columnas) && columnas > 0)
+        {
+            MatrizCeros matrizCeros = new MatrizCeros(filas, columnas);
+            matrizCeros.LlenarMatriz();
+            matrizCeros.CalcularCerosEnFilas();
+        }
+        else
+        {
+        Console.WriteLine("Número de columnas no válido. Debe ser un entero positivo.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Número de filas no válido. Debe ser un entero positivo.");
+    }
+class MatrizCeros
+{
+    private int[,] matriz;
+
+    public MatrizCeros(int filas, int columnas)
+    {
+        matriz = new int[filas, columnas];
+    }
+
+    public void LlenarMatriz()
+    {
+        int filas = matriz.GetLength(0);
+        int columnas = matriz.GetLength(1);
+
+        Console.WriteLine("Ingresa los valores de la matriz:");
+
+        for (int fila = 0; fila < filas; fila++)
+        {
+            for (int columna = 0; columna < columnas; columna++)
+            {
+                Console.Write($"Elemento [{fila},{columna}]: ");
+                if (int.TryParse(Console.ReadLine(), out int valor))
+                {
+                    matriz[fila, columna] = valor;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no válida. Ingresa un número entero.");
+                    columna--; // Reintentar la entrada para esta celda
+                }
+            }
+        }
+    }
+
+    public void CalcularCerosEnFilas()
+    {
+        int filas = matriz.GetLength(0);
+
+        Console.WriteLine("Cantidad de ceros en cada fila:");
+
+        for (int fila = 0; fila < filas; fila++)
+        {
+            int contadorCeros = 0;
+
+            for (int columna = 0; columna < matriz.GetLength(1); columna++)
+            {
+                if (matriz[fila, columna] == 0)
+                {
+                    contadorCeros++;
+                }
+            }
+
+            Console.WriteLine($"Fila {fila + 1}: {contadorCeros} ceros");
+        }
+    }
+}
---
+++ Practica_8/Program.cs
@@ -0,0 +1,99 @@
+﻿Console.Write("Ingrese el tamaño del cuadro magico: ");
+int tamaño = int.Parse(Console.ReadLine()!);
+
+CuadroMagico cuadroMagico = new CuadroMagico(tamaño);
+
+cuadroMagico.leerMatriz();
+
+if(cuadroMagico.esCuadroMagico())
+{
+    Console.WriteLine($"Es un cuadro mágico con constante mágica: {cuadroMagico.constanteMagica()}");
+}
+else
+{
+    Console.WriteLine("No es un cuadro mágico.");
+}
+
+class CuadroMagico
+{
+    private int[,] matriz;
+    private int tamaño;
+
+    public CuadroMagico(int tamaño)
+    {
+        this.tamaño = tamaño;
+        matriz = new int[tamaño,tamaño];
+    }
+
+    public void leerMatriz()
+    {
+        Console.WriteLine($"Ingrese los valores para el cuadro mágico ({tamaño}x{tamaño}: )");
+
+        for (int i = 0; i < tamaño; i++ )
+        {
+            for (int j = 0; j < tamaño; j++)
+            {
+                Console.Write($"Fila {i + 1}, columna {j + 1}: ");
+                matriz[i,j] = int.Parse(Console.ReadLine()!);
+            }
+        }
+    }
+
+    public bool esCuadroMagico()
+    {
+        int sumarReferencia = 0;
+        for (int j = 0; j < tamaño; j++)
+        {
+            sumarReferencia += matriz[0,j];
+        }
+
+        for (int i = 0; i < tamaño; i++)
+        {
+            int sumaFila = 0;
+            for (int j = 0; j < tamaño; j++)
+            {
+                sumaFila += matriz[i,j];
+            }
+            if(sumaFila != sumarReferencia)
+            {
+                return false;
+            }
+        }
+        for (int j  = 0; j < tamaño; j++)
+        {
+            int sumaColumna = 0;
+            for(int i = 0; i < tamaño; i++)
+            {
+                sumaColumna += matriz[i,j];
+
+            }
+            if(sumaColumna != sumarReferencia)
+            {
+                return false;
+            }
+        }
+
+        int sumarDiagonalPrincipal = 0;
+        for (int i = 0; i < tamaño; i++)
+        {
+            sumarDiagonalPrincipal +=matriz[i,i];
+        }
+        if(sumarDiagonalPrincipal != sumarReferencia)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int constanteMagica()
+    {
+        int sumarReferencia = 0;
+        for (int j = 0; j < tamaño; j++)
+        {
+            sumarReferencia += matriz[0,j];
+
+        }
+        return sumarReferencia;
+    }
+}
